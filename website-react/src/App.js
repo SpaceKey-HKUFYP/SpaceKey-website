bc4b1c028d2fe04d3665693074b66befd0b4b610
Wang Jikun <wagjk@hku.hk>
2019-01-07 17:13:15 +0800

    update dependencies: add react-router-dom

--- website-react/src/App.js
+++ website-react/src/App.js
@@ -1,19 +1,21 @@
-import React, { Component } from 'react';
-import { Input } from 'semantic-ui-react';
+import React from 'react';
+import { render, ReactDOM } from 'react-dom';
+import { Container, Divider, Header, Message } from "semantic-ui-react";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
-// import logo from './logo.svg';
-// import './App.css';
+import './index.css';
+import App from './components/App';
+import FilterBar from './components/FilterBar';
+import * as serviceWorker from './serviceWorker';
 
-class App extends Component {
-  render() {
-    const InputExampleInput = () => <Input placeholder='Search...'/>
-    return (
-      <Input placeholder='NICE...'/>
-    );
-  }
-}
+//ReactDOM.render(<App />, document.getElementById('root'));
 
+render(
+  <FilterBar />,
+  document.getElementById("root")
+);
 
-
-export default App;
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: http://bit.ly/CRA-PWA
+serviceWorker.unregister();
