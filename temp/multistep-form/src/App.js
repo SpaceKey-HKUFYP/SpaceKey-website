cb41f8c6a65eb71d7795c2a2d105690794602f2e
Wang Jikun <wagjk@hku.hk>
2019-01-07 14:46:17 +0800

    upload template

---
+++ temp/multistep-form/src/App.js
@@ -0,0 +1,17 @@
+// app.js
+import React, { Component } from 'react';
+import './App.css';
+import MainForm from './components/MainForm';
+import { Container } from 'semantic-ui-react';
+
+class App extends Component {
+
+  render() {
+    return(
+      <Container textAlign='center'>
+        <MainForm />
+      </Container>     )
+  }
+}
+
+export default App;
---
+++ temp/multistep-form/src/components/Success.jsx
@@ -0,0 +1,14 @@
+// Success.jsx
+import React, { Component } from 'react';
+
+class Success extends Component{
+    render(){
+        return(
+            <div>
+                <h1 className="ui centered">Details Successfully Saved</h1>
+            </div>
+        )
+    }
+}
+
+export default Success;
