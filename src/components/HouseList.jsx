5e738fa03f0bb393bc71414dd40f270228f9d436
budiman <budiman_1597@hotmail.com>
2019-01-12 18:26:27 +0800

    Testing sticky for search

--- src/components/HouseList.jsx
+++ src/components/HouseList.jsx
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
 import { Segment, Grid, Image, Button, Header } from "semantic-ui-react";
 import "../layout.css";
-import MapContainer from "./MapContainer";
 
 // address: "No.2 Tai Pak Terrace, Kennedy Town",
 // imageURL: "images/house1.jpg",
@@ -73,12 +72,8 @@ class HouseList extends Component {
 
     return (
       <Grid columns="equal" divided={false} padded={false} fluid="true">
-        <Grid.Column>{listOfHouse}</Grid.Column>
-        <Grid.Column>
-          <Segment style={{ height: "500px" }}>
-            <MapContainer data={this.props.data} />
-          </Segment>
-        </Grid.Column>
+        <Grid.Column width={6}>{listOfHouse}</Grid.Column>
+        <Grid.Column width={10} />
       </Grid>
     );
   }
--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -8,11 +8,13 @@ import {
   Container,
   Header,
   Rail,
-  Sticky
+  Sticky,
+  Divider
 } from "semantic-ui-react";
 import { SpmFilter, ScrollFilter } from "./FilterMenu";
 import NavigationBar from "./NavigationBar";
 import HouseList from "./HouseList";
+import MapContainer from "./MapContainer";
 import axios from "axios";
 
 import "../layout.css";
@@ -449,57 +451,68 @@ class Search extends Component {
             <Container>
               <Grid
                 padded
-                divided
+                divided={false}
                 fluid="true"
                 style={{ height: "100%" }}
                 verticalAlign="middle"
               >
-                <Grid.Column style={{ maxWidth: 1000 }}>
-                  <Segment>
-                    <Header as="h3">Property for rent in {where}</Header>
+                <Grid.Row>
+                  <Grid.Column style={{ maxWidth: "1400px" }}>
+                    <Segment>
+                      <Header as="h3">Property for rent in {where}</Header>
 
-                    <ScrollFilter
-                      handler={bedrooms.handler}
-                      data={bedrooms.data}
-                      status={bedrooms.status}
-                    />
+                      <ScrollFilter
+                        handler={bedrooms.handler}
+                        data={bedrooms.data}
+                        status={bedrooms.status}
+                      />
 
-                    <ScrollFilter
-                      handler={saleableArea.handler}
-                      data={saleableArea.data}
-                      status={saleableArea.status}
-                    />
+                      <ScrollFilter
+                        handler={saleableArea.handler}
+                        data={saleableArea.data}
+                        status={saleableArea.status}
+                      />
 
-                    <ScrollFilter
-                      handler={grossArea.handler}
-                      data={grossArea.data}
-                      status={grossArea.status}
-                    />
+                      <ScrollFilter
+                        handler={grossArea.handler}
+                        data={grossArea.data}
+                        status={grossArea.status}
+                      />
 
-                    <ScrollFilter
-                      handler={price.handler}
-                      data={price.data}
-                      status={price.status}
-                    />
+                      <ScrollFilter
+                        handler={price.handler}
+                        data={price.data}
+                        status={price.status}
+                      />
 
-                    <Button
-                      onClick={() => general.handler.openHandler("spm", true)}
-                    >
-                      SPM
-                    </Button>
-                    <SpmFilter
-                      data={spm.data}
-                      handler={spm.handler}
-                      status={spm.status}
-                      size="small"
-                    />
-                  </Segment>
-                </Grid.Column>
+                      <Button
+                        onClick={() => general.handler.openHandler("spm", true)}
+                      >
+                        SPM
+                      </Button>
+                      <SpmFilter
+                        data={spm.data}
+                        handler={spm.handler}
+                        status={spm.status}
+                        size="small"
+                      />
+                    </Segment>
+                  </Grid.Column>
+                </Grid.Row>
+                <Grid.Row>
+                  <Grid.Column width={6} />
+                  <Grid.Column width={10}>
+                    <Segment style={{ height: "500px", padding: "0px" }}>
+                      <MapContainer data={general.data.filteredHouse} />
+                    </Segment>
+                  </Grid.Column>
+                </Grid.Row>
               </Grid>
             </Container>
           </Sticky>
         </Rail>
         <Container style={{ paddingTop: "265.9px" }}>
+          <Divider hidden />
           <Grid
             columns="equal"
             divided
@@ -508,7 +521,7 @@ class Search extends Component {
             style={{ height: "100%" }}
             verticalAlign="middle"
           >
-            <Grid.Column style={{ maxWidth: 1000 }}>
+            <Grid.Column style={{ maxWidth: "1400px" }}>
               <HouseList data={general.data.filteredHouse} />
             </Grid.Column>
           </Grid>
